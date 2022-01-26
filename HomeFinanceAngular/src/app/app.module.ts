4692208b6457ef16e2f24ff47e33ab07c0c68dfd
Kirill Kozlov <kirill.kozlov.fhaachen@gmail.com>
2022-01-26 22:48:00 +0100

    init angular project

---
+++ HomeFinanceAngular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
