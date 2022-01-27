41e2576b9d6c247effd3bdccdd315304884727da
Kirill Kozlov <kirill.kozlov.fhaachen@gmail.com>
2022-01-27 15:03:18 +0100

    RegistrationForm

--- HomeFinanceAngular/src/app/user/registration/registration.component.ts
+++ HomeFinanceAngular/src/app/user/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-registration',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: UserService) { }
 
   ngOnInit(): void {
   }
