e48b1b3ec29c141326a7729215d19633e5056c11
Kirill Kozlov <kirill.kozlov@island-labs.com>
2023-02-17 16:50:58 +0100

    select wallet open csv

--- HomeFinance.Import.Desktop/MainViewModel.cs
+++ HomeFinance.Import.Desktop/MainViewModel.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using HomeFinance.DataAccess.Proxy;
+using HomeFinance.Domain.DomainModels;
+using HomeFinance.Domain.Utils;
+using Microsoft.Win32;
 
 namespace HomeFinance.Import.Desktop
 {
@@ -34,40 +41,55 @@ namespace HomeFinance.Import.Desktop
             }
         }
 
+        public bool IsLoggedIn
+        {
+            get => _isLoggedIn;
+            set
+            {
+                _isLoggedIn = value;
+                this.OnPropertyChanged();
+            }
+        }
+
         public RelayCommand ButtonClickCommand { get; set; }
 
         private bool _isLoggedIn = false;
         private string _username = string.Empty;
         private string _password = string.Empty;
 
-        public string ButtonText => _isLoggedIn ? "Logout" : "Login";
+        public string ButtonText => IsLoggedIn ? "Logout" : "Login";
 
-        public LoginViewModel()
+        private Action _onSuccessfullyLogin;
+
+        public LoginViewModel(Action onSuccessfullyLogin)
         {
+            this._onSuccessfullyLogin = onSuccessfullyLogin;
             this.ButtonClickCommand = new RelayCommand(o => ButtonClick(), o => CanClick());
         }
         
         public void ButtonClick()
         {
-            if (_isLoggedIn)
+            if (!IsLoggedIn)
             {
                 this.Login();
+                this._onSuccessfullyLogin();
             }
             else
             {
-                this._isLoggedIn = false;
+                this.IsLoggedIn = false;
             }
             this.OnPropertyChanged(nameof(ButtonText));
         }
 
         public void Login()
         {
-            this._isLoggedIn = true;
+            Client.Instance.Login(Username, Password);
+            this.IsLoggedIn = true;
         }
 
         public bool CanClick()
         {
-            return _isLoggedIn ||
+            return IsLoggedIn ||
                    (!string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password));
             
         }
@@ -79,8 +101,90 @@ namespace HomeFinance.Import.Desktop
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
+
+
+    class WalletViewModel
+    {
+        public Wallet Wallet { get; }
+        public string DisplayName => $"{this.Wallet.Name} ({this.Wallet.GroupName})";
+
+        public WalletViewModel(Wallet wallet)
+        {
+            this.Wallet = wallet;
+        }
+    }
+
+    class SelectWalletViewModel
+    {
+        readonly ObservableCollection<WalletViewModel> _wallets=new ();
+        public IEnumerable<WalletViewModel> Wallets => _wallets;
+
+        public Wallet SelectedWallet { get; set; }
+
+        public async void UpdateWallets()
+        {
+            var walletsToAdd = await Client.Instance.Gateway.WalletRepository.GetAll();
+            this._wallets.Clear();
+            walletsToAdd.ToList().ForEach(w=> _wallets.Add(new WalletViewModel(w)));
+        }
+    }
+
+    class CSVViewModel
+    {
+        public ICommand OpenCSVCommand { get; }
+
+        public CSVViewModel()
+        {
+            this.OpenCSVCommand = new RelayCommand(_ => this.OpenCSV());
+        }
+
+        void OpenCSV()
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                
+            }
+            
+        }
+    }
+
     class MainViewModel
     {
-        public LoginViewModel LoginViewModel { get; set; }=new LoginViewModel();
+        public LoginViewModel LoginViewModel { get; set; }
+        public SelectWalletViewModel SelectWalletViewModel { get; set; } = new SelectWalletViewModel();
+        public CSVViewModel CSVViewModel { get; set; } = new CSVViewModel();
+
+        public MainViewModel()
+        {
+            LoginViewModel = new LoginViewModel(SelectWalletViewModel.UpdateWallets);
+        }
+    }
+
+    public class Client
+    {
+        private Client()
+        {
+
+        }
+
+        public static Client Instance=new Client();
+
+        private IGateway? _gateway = null;
+        public IGateway Gateway => this._gateway ?? throw new NullReferenceException();
+
+        public void Login(string username, string password)
+        {
+            this._gateway=new Gateway("https://localhost:7080/api/", username, password);
+        }
+
+        public void Logout()
+        {
+            this._gateway = null;
+        }
+
     }
 }
