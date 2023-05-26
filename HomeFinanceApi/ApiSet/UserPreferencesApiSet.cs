eef9c8d67b90d876b681b2b29cc61872af513679
Kirill Kozlov <kirill.kozlov.fhaachen@gmail.com>
2023-05-26 22:02:55 +0200

    Added FixTime api

--- HomeFinanceApi/ApiSet/UserPreferencesApiSet.cs
+++ HomeFinanceApi/ApiSet/UserPreferencesApiSet.cs
@@ -17,6 +17,7 @@ public static class UserPreferencesApiSet
         app.MapGet("api/preferences/", Get);
         app.MapGet("api/preferences/updatedates", UpdateDates);
         app.MapGet("api/preferences/addday", AddDayToRepeatable);
+        app.MapGet("api/preferences/fixTimes", FixTimes);
         app.MapPost("api/preferences/", Post);
         app.MapPut("api/preferences/", Put);
     }
@@ -44,6 +45,22 @@ public static class UserPreferencesApiSet
         var newOps = allOps.Select(i => new RepeatableOperation(i.Id, i.WalletId, i.OperationType, i.Tags, i.Amount, i.Comment, i.WalletToId, i.NextExecution.Date.AddDays(1), i.RepeatableType));
         await unitOfWork.RepeatableOperationRepository.Update(newOps);
     }
+
+    [Authorize]
+    static async Task FixTimes(IGateway unitOfWork)
+    {
+        {
+            var allOps = (await unitOfWork.OperationRepository.GetAll()).Where(i => i.DateTime.Hour != 0).ToList();
+            var newOps = allOps.Select(i => new Operation(i.Id, i.WalletId, i.OperationType, i.Tags, i.Amount, i.Comment, i.WalletToId, i.DateTime.AddHours(24 - i.DateTime.Hour)));
+            await unitOfWork.OperationRepository.Update(newOps);
+        }
+        {
+            var allOps = await unitOfWork.RepeatableOperationRepository.GetAll();
+            var newOps = allOps.Select(i => new RepeatableOperation(i.Id, i.WalletId, i.OperationType, i.Tags, i.Amount, i.Comment, i.WalletToId, i.NextExecution.AddHours(24 - i.NextExecution.Hour), i.RepeatableType));
+            await unitOfWork.RepeatableOperationRepository.Update(newOps);
+        }
+    }
+
 
     [Authorize]
     static async Task Post(UserPreferencesDto preferences, IGateway unitOfWork)
